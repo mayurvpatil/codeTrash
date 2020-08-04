4110aab35d19c5a207cdddd796dd3df2c2920352
mayur patil <mayurvpatil>
2020-08-05 02:08:57 +0530

    modified template for simplycity

--- template.cpp
+++ template.cpp
@@ -1,35 +1,11 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <cstring>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <random>
-#include <set>
-#include <stack>
-#include <vector>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
-// BEGIN NO SAD
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-typedef vector<int> vi;
-// END NO SAD
-
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<vector<ll>> matrix;
-typedef pair<ll, ll> pll;
-
-
 void solve() {
-   
+    cout << "ohh";   
 }
 
 void testCasesIterate() {
@@ -40,12 +16,11 @@ void testCasesIterate() {
     solve();
     cout << "\n";
   }
-  
-  
 }
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
   testCasesIterate();
+  //solve();
 }
