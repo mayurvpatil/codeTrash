8eb19549af121475765c99c13ef49170ce44902e
mayur patil <mayurvpatil>
2020-08-04 20:53:19 +0530

    Initial cpp template

---
+++ template.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+// BEGIN NO SAD
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+// END NO SAD
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<vector<ll>> matrix;
+typedef pair<ll, ll> pll;
+
+
+void solve() {
+   
+}
+
+void testCasesIterate() {
+  int t;
+  cin >> t;
+  for(int casenum = 1; casenum <= t; casenum++) {
+    cout << "Case #" << casenum << ": ";
+    solve();
+    cout << "\n";
+  }
+  
+  
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  testCasesIterate();
+}
